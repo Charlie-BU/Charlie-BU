e10cfc739b41dcd270eb78a38f79f3fbdc70604c
Charlie.BU <15947513567@163.com>
2025-07-02 11:24:59 +0800

    m

---
+++ BE-Charlie/models.py
@@ -0,0 +1,304 @@
+import re
+from datetime import datetime
+from sqlalchemy import create_engine, ForeignKey, Boolean, Column, Integer, Text, DateTime, Date, Float, JSON
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy.ext.mutable import MutableList
+from bcrypt import hashpw, gensalt, checkpw
+import random
+
+from config import DATABASE_URI
+
+engine = create_engine(DATABASE_URI, echo=True)
+# 数据库表基类
+Base = declarative_base()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+Base.metadata.naming_convention = naming_convention
+# 会话，用于通过ORM操作数据库
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# 全局session会造成严重后果
+# session = Session()
+
+
+# 浏览器指纹
+class Fingerprint(Base):
+    __tablename__ = "fingerprint"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fingerprint = Column(Text, nullable=False)
+    create_time = Column(DateTime, default=datetime.now)
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "fingerprint": self.fingerprint,
+            "create_time": self.create_time,
+        }
+        return data
+
+
+class Admin(Base):
+    __tablename__ = "admin"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text, nullable=False)
+    password = Column(Text, nullable=False)
+
+    @staticmethod
+    def hash_password(password):
+        hashed = hashpw(password.encode("utf-8"), gensalt())
+        return hashed.decode("utf-8")
+
+    def check_password(self, password):
+        return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "name": self.name,
+            "password": self.password,
+        }
+        return data
+
+
+class Charlie(Base):
+    __tablename__ = "charlie"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text)
+    name_ENG = Column(Text)
+    description = Column(Text)
+    description_ENG = Column(Text)
+    height = Column(Float)
+    weight = Column(Float)
+    birthday = Column(Date)
+    github = Column(Text)
+    email = Column(Text)
+    motto = Column(MutableList.as_mutable(JSON()), default=[])
+    motto_ENG = Column(MutableList.as_mutable(JSON()), default=[])
+    photos = Column(MutableList.as_mutable(JSON()), default=[])
+    resume = Column(Text)
+
+    visitorNumber = Column(Integer)
+
+    @property
+    def age(self):
+        return (datetime.now().date() - self.birthday).days // 365
+
+    def to_json(self):
+        selected_motto = random.choice(self.motto) if self.motto else None
+        data = {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "age": self.age,
+            "height": self.height,
+            "weight": self.weight,
+            "birthday": self.birthday,
+            "github": self.github,
+            "email": self.email,
+            "motto": selected_motto,
+            "photos": self.photos,
+            "resume": self.resume,
+            "visitorNumber": self.visitorNumber,
+        }
+        return data
+
+    def to_json_ENG(self):
+        selected_motto = random.choice(self.motto_ENG) if self.motto_ENG else None
+        data = {
+            "id": self.id,
+            "name": self.name_ENG,
+            "description": self.description_ENG,
+            "age": self.age,
+            "height": self.height,
+            "weight": self.weight,
+            "birthday": self.birthday,
+            "github": self.github,
+            "email": self.email,
+            "motto": selected_motto,
+            "photos": self.photos,
+            "resume": self.resume,
+            "visitorNumber": self.visitorNumber,
+        }
+        return data
+
+
+class Talent(Base):
+    __tablename__ = "talent"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(Text)
+    title_ENG = Column(Text)
+    description = Column(Text)
+    description_ENG = Column(Text)
+    icon = Column(Text)
+    gotoUrl = Column(Text)
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "icon": self.icon,
+            "gotoUrl": self.gotoUrl,
+        }
+        return data
+
+    def to_json_ENG(self):
+        data = {
+            "id": self.id,
+            "title": self.title_ENG,
+            "description": self.description_ENG,
+            "icon": self.icon,
+            "gotoUrl": self.gotoUrl,
+        }
+        return data
+
+
+class Achievement(Base):
+    __tablename__ = "achievement"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    label = Column(Text)
+    label_ENG = Column(Text)
+    number = Column(Text)
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "label": self.label,
+            "number": self.number,
+        }
+        return data
+
+    def to_json_ENG(self):
+        data = {
+            "id": self.id,
+            "label": self.label_ENG,
+            "number": self.number,
+        }
+        return data
+
+
+class GrowthTimeline(Base):
+    __tablename__ = "growth_timeline"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    content = Column(Text)
+    content_ENG = Column(Text)
+    timestamp = Column(Text)
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "content": self.content,
+            "timestamp": self.timestamp,
+        }
+        return data
+
+    def to_json_ENG(self):
+        data = {
+            "id": self.id,
+            "content": self.content_ENG,
+            "timestamp": self.timestamp,
+        }
+        return data
+
+
+class Bubble(Base):
+    __tablename__ = "bubble"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    content = Column(Text)
+    content_ENG = Column(Text)
+    date = Column(Date)
+    tags = Column(MutableList.as_mutable(JSON()), default=[])
+    tag_ENG = Column(MutableList.as_mutable(JSON()), default=[])
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "content": self.content,
+            "date": self.date,
+            "tags": self.tags,
+        }
+        return data
+
+    def to_json_ENG(self):
+        data = {
+            "id": self.id,
+            "content": self.content_ENG,
+            "date": self.date,
+            "tags": self.tag_ENG,
+        }
+        return data
+
+
+class Article(Base):
+    __tablename__ = "article"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    content = Column(Text)
+    content_ENG = Column(Text)
+    timeCreated = Column(DateTime, default=datetime.now)
+    timeLastUpdated = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    tags = Column(MutableList.as_mutable(JSON()), default=[])
+    tag_ENG = Column(MutableList.as_mutable(JSON()), default=[])
+    # 文本：1；markdown：2
+    type = Column(Integer, default=1)
+    isReleased = Column(Boolean, default=False)
+
+    @property
+    def title(self):
+        if not self.content:
+            return ""
+        if self.type == 1:
+            return self.content.split("\n")[0]
+        elif self.type == 2:
+            title = self.content.split("\n")[0]
+            title = re.sub(r"^#+\s*", "", title)
+            return title
+        return ""
+
+    @property
+    def title_ENG(self):
+        if not self.content_ENG:
+            return ""
+        if self.type == 1:
+            return self.content_ENG.split("\n")[0]
+        elif self.type == 2:
+            title = self.content_ENG.split("\n")[0]
+            title = re.sub(r"^#+\s*", "", title)
+            return title
+        return ""
+
+    def to_json(self):
+        data = {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "timeCreated": self.timeCreated,
+            "timeLastUpdated": self.timeLastUpdated,
+            "tags": self.tags,
+            "type": self.type,
+            "isReleased": self.isReleased,
+        }
+        return data
+
+    def to_json_ENG(self):
+        data = {
+            "id": self.id,
+            "title": self.title_ENG,
+            "content": self.content_ENG,
+            "timeCreated": self.timeCreated,
+            "timeLastUpdated": self.timeLastUpdated,
+            "tags": self.tag_ENG,
+            "type": self.type,
+            "isReleased": self.isReleased,
+        }
+        return data
+
+
+# 创建所有表（被alembic替代）
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
---
+++ BE-Charlie/subRouters/api.py
@@ -0,0 +1,525 @@
+import json
+import time
+
+from robyn import SubRouter, jsonify
+from dateutil import parser
+
+from models import *
+import utils
+
+apiRouter = SubRouter(__file__, prefix="/api")
+
+
+@apiRouter.post("/new_visitor")
+async def new_visitor(request):
+    session = Session()
+    data = request.json()
+    # 访客数量操作
+    fingerprint = data.get("fingerprint")
+    if fingerprint:
+        exist_fingerprint = session.query(Fingerprint).filter(Fingerprint.fingerprint == fingerprint).first()
+        if not exist_fingerprint:
+            new_fingerprint = Fingerprint(fingerprint=fingerprint)
+            session.add(new_fingerprint)
+            charlie = session.query(Charlie).first()
+            charlie.visitorNumber += 1
+            session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/login")
+async def login(request):
+    session = Session()
+    data = request.json()
+    username = data.get("username")
+    admin = session.query(Admin).filter(Admin.name == username).first()
+    if not admin:
+        return jsonify({
+            "status": 401,
+            "message": "用户不存在"
+        })
+    password = data.get("password")
+    if not admin.check_password(password):
+        return jsonify({
+            "status": 402,
+            "message": "密码错误"
+        })
+    sessionid = utils.generate_sessionid(admin.id)
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "登录成功",
+        "sessionid": sessionid
+    })
+
+
+@apiRouter.post("/check_sessionid")
+async def check_sessionid(request):
+    data = request.json()
+    sessionid = data.get("sessionid")
+    res = utils.check_sessionid(sessionid)
+    admin_id = res["user_id"] if res else None
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "admin_id": admin_id
+    })
+
+
+@apiRouter.post("/get_charlie")
+async def get_charlie(request):
+    session = Session()
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    charlie = session.query(Charlie).first()
+    session.commit()
+    charlie = charlie.to_json() if lang == "Chinese" else charlie.to_json_ENG()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "charlie": charlie
+    })
+
+
+@apiRouter.post("/get_talents")
+async def get_talents(request):
+    session = Session()
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    talents = session.query(Talent).all()
+    talents = [talent.to_json() if lang == "Chinese" else talent.to_json_ENG() for talent in talents]
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "talents": talents
+    })
+
+
+@apiRouter.post("/delete_talent")
+async def delete_talent(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    talent = session.query(Talent).get(id)
+    if not talent:
+        return jsonify({
+            "status": 404,
+            "message": "Talent not found",
+        })
+    session.delete(talent)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/get_achievements")
+async def get_achievements(request):
+    session = Session()
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    achievements = session.query(Achievement).all()
+    achievements = [achievement.to_json() if lang == "Chinese" else achievement.to_json_ENG() for achievement in
+                    achievements]
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "achievements": achievements
+    })
+
+
+@apiRouter.post("/delete_achievement")
+async def delete_achievement(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    achievement = session.query(Achievement).get(id)
+    if not achievement:
+        return jsonify({
+            "status": 404,
+            "message": "Achievement not found",
+        })
+    session.delete(achievement)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/get_growthTimeline")
+async def get_growthTimeline(request):
+    session = Session()
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    growthTimelines = session.query(GrowthTimeline).all()
+    growthTimelines = [growthTimeline.to_json() if lang == "Chinese" else growthTimeline.to_json_ENG() for
+                       growthTimeline in
+                       growthTimelines]
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "growthTimelines": growthTimelines
+    })
+
+
+@apiRouter.post("/delete_growthTimeline")
+async def delete_growthTimeline(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    growthTimeline = session.query(GrowthTimeline).get(id)
+    if not growthTimeline:
+        return jsonify({
+            "status": 404,
+            "message": "GrowthTimeline not found",
+        })
+    session.delete(growthTimeline)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/get_bubbles")
+async def get_bubbles(request):
+    session = Session()
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    bubbles = session.query(Bubble).all()
+    bubbles = [bubble.to_json() if lang == "Chinese" else bubble.to_json_ENG() for
+               bubble in
+               bubbles]
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "bubbles": bubbles
+    })
+
+
+@apiRouter.post("/delete_bubble")
+async def delete_bubble(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    bubble = session.query(Bubble).get(id)
+    if not bubble:
+        return jsonify({
+            "status": 404,
+            "message": "Bubble not found",
+        })
+    session.delete(bubble)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/get_articles")
+async def get_articles(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        query = session.query(Article).filter(Article.isReleased)
+    else:
+        query = session.query(Article)
+    data = request.json()
+    lang = data.get("lang", "Chinese")
+    articles = query.order_by(Article.timeLastUpdated.desc()).all()
+    articles = [{
+                    "id": article.id,
+                    "title": article.title_ENG,
+                    "timeCreated": article.timeCreated,
+                    "timeLastUpdated": article.timeLastUpdated,
+                    "tags": article.tag_ENG,
+                    "type": article.type,
+                    "isReleased": article.isReleased,
+                } if lang == "English" else {
+        "id": article.id,
+        "title": article.title,
+        "timeCreated": article.timeCreated,
+        "timeLastUpdated": article.timeLastUpdated,
+        "tags": article.tags,
+        "type": article.type,
+        "isReleased": article.isReleased,
+    }
+                for article in articles]
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "articles": articles
+    })
+
+
+@apiRouter.post("/get_article_content")
+async def get_article_content(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        query = session.query(Article).filter(Article.isReleased)
+    else:
+        query = session.query(Article)
+    data = request.json()
+    id = data.get("id")
+    lang = data.get("lang", "Chinese")
+
+    article = query.filter(Article.id == id).first()
+    if not article:
+        return jsonify({
+            "status": 404,
+            "message": "Article not found",
+        })
+    article = article.to_json() if lang == "Chinese" else article.to_json_ENG()
+
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "article": article
+    })
+
+
+@apiRouter.post("/change_article_status")
+async def change_article_status(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    article = session.query(Article).get(id)
+    if not article:
+        return jsonify({
+            "status": 404,
+            "message": "Article not found",
+        })
+    article.isReleased = not article.isReleased
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/get_article_detail/")
+async def get_article_detail(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    article = session.query(Article).get(id)
+    if not article:
+        return jsonify({
+            "status": 404,
+            "message": "Article not found",
+        })
+    article = {
+        "id": article.id,
+        "title": article.title,
+        "content": article.content,
+        "content_ENG": article.content_ENG,
+        "timeCreated": article.timeCreated,
+        "timeLastUpdated": article.timeLastUpdated,
+        "tags": article.tags,
+        "tag_ENG": article.tag_ENG,
+        "type": article.type,
+        "isReleased": article.isReleased,
+    }
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+        "article": article
+    })
+
+
+@apiRouter.post("/update_article")
+async def update_article(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    article = session.query(Article).get(id)
+    if not article:
+        return jsonify({
+            "status": 404,
+            "message": "Article not found",
+        })
+    fields = ["type", "content", "content_ENG", "tags", "tag_ENG"]
+    is_updated = False
+    for field in fields:
+        if getattr(article, field) != data.get(field):
+            if field == "type" and getattr(article, field) == int(data.get(field)):
+                continue
+            if field == "tags":
+                if getattr(article, field) != json.loads(data.get(field)):
+                    if data.get(field) and data.get(field) != "":
+                        article.tags = json.loads(data.get(field))
+                        is_updated = True
+                        continue
+                    else:
+                        continue
+                else:
+                    continue
+            if field == "tag_ENG":
+                if getattr(article, field) != json.loads(data.get(field)):
+                    if data.get(field) and data.get(field) != "":
+                        article.tag_ENG = json.loads(data.get(field))
+                        is_updated = True
+                        continue
+                    else:
+                        continue
+                else:
+                    continue
+            setattr(article, field, data.get(field))
+            is_updated = True
+    if not is_updated:
+        return jsonify({
+            "status": -1,
+            "message": "没有更新的内容",
+        })
+    isReleased = True if data.get("isReleased") == "true" else False
+    article.isReleased = isReleased
+    article.timeLastUpdated = datetime.now()
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/add_article")
+async def add_article(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    type = int(data.get("type"))
+    content = data.get("content")
+    content_ENG = data.get("content_ENG")
+    tags = data.get("tags")
+    if tags:
+        tags = json.loads(tags)
+    tag_ENG = data.get("tag_ENG")
+    if tag_ENG:
+        tag_ENG = json.loads(tag_ENG)
+    article = Article(type=type, content=content, content_ENG=content_ENG, timeCreated=datetime.now(), tags=tags,
+                      tag_ENG=tag_ENG,
+                      timeLastUpdated=datetime.now())
+    isReleased = True if data.get("isReleased") == "true" else False
+    article.isReleased = isReleased
+    session.add(article)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
+
+
+@apiRouter.post("/delete_article")
+async def delete_article(request):
+    session = Session()
+    headers = request.headers
+    cookie = utils.parse_cookie_string(headers.get("cookie"))
+    sessionid = cookie.get("sessionid")
+    if not cookie or not sessionid or not utils.check_sessionid(sessionid):
+        return jsonify({
+            "status": 403,
+            "message": "No permission",
+        })
+    data = request.json()
+    id = data.get("id")
+    article = session.query(Article).get(id)
+    if not article:
+        return jsonify({
+            "status": 404,
+            "message": "Article not found",
+        })
+    session.delete(article)
+    session.commit()
+    session.close()
+    return jsonify({
+        "status": 200,
+        "message": "success",
+    })
