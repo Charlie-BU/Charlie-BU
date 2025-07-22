5d1ba213fb6d66fc6f32f9f4b69ba581eb28fc9a
Charlie.BU <15947513567@163.com>
2025-07-22 19:25:54 +0800

    chore

---
+++ BE-Charlie/migrations/versions/e17cc8215755_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e17cc8215755
+Revises: 18c576dba598
+Create Date: 2025-07-22 19:05:08.245122
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e17cc8215755'
+down_revision = '18c576dba598'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
